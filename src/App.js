a0365fccf0b5d14482ea86f16b5b5c9f623111da
ShayanAkhtar <shayantanoli38@gmail.com>
2024-07-31 11:08:34 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount]=useState(0);
+
+  const increment=()=>{
+    setCount(count+1);
+  };
+  const decrement=()=>
+  {
+    setCount(count>0?count-1:0);
+  }
+  const reset=()=>
+  {
+    setCount(0);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Water Counter</h1>
+        <p>Number of glasses of water you drank today:</p>
+        <div className="counter">
+          <button onClick={decrement}>-</button>
+          <span>{count}</span>
+          <button onClick={increment}>+</button>
+        </div>
+        <button onClick={reset} className="reset-button">Reset</button>
       </header>
+      <footer>
+        <h5>Developed By @Shayan Akhtar</h5>
+      </footer>
     </div>
+    
   );
 }
 
